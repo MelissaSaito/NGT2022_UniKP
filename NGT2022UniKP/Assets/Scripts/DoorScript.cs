22a183b229c6ea5714704154af96330ff38946c3
kitei.dai@gmail.com <kitei.dai@gmail.com>
2022-03-13 21:59:49 +0900

    KeyScriptの作成と当たり判定の調整/ドアを開けるという判定➤キテイ

---
+++ NGT2022UniKP/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject key;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = GameObject.Find("Key");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (key.GetComponent<KeyScript>().GetKey)
+        {
+            if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+}
+
