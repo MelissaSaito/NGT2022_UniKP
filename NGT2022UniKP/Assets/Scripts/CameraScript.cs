623c35929a85d799168199d2969774f0ce17775b
大栗 <guri.1123.y@gmail.com>
2022-03-15 15:52:41 +0900

    コントローラー実装（鍵と宝の取得）

--- NGT2022UniKP/Assets/Scripts/CameraScript.cs
+++ NGT2022UniKP/Assets/Scripts/CameraScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
     GameObject targetObj;
     Vector3 targetPos;
 
--- NGT2022UniKP/Assets/Scripts/PlayerScript.cs
+++ NGT2022UniKP/Assets/Scripts/PlayerScript.cs
@@ -45,7 +45,7 @@ public class PlayerScript : MonoBehaviour
         Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
 
         //slow移動
-        if (Input.GetKey(KeyCode.C))
+        if (Input.GetKey(KeyCode.C) || Input.GetButton("ControllerB"))
         {
             // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
             rb.velocity = moveForward * slowMoveSpeed + new Vector3(0, rb.velocity.y, 0);
