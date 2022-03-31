ab432ffd1ec09c0bf76cbfc64059074cdccbc99d
UchimuraYousuke <yousukeuchimura8113@gmail.com>
2022-03-31 12:11:59 +0900

    徘徊enemy追加、テキスト表示→内村

--- NGT2022UniKP/Assets/Scripts/RoamingEnemyScript.cs
+++ NGT2022UniKP/Assets/Scripts/RoamingEnemyScript.cs
@@ -11,6 +11,22 @@ public class RoamingEnemyScript : MonoBehaviour
     private int currentWaypointIndex;
     private float enemySpeed = 3.0f;
 
+    //03/30内村追加-------------------------------------
+    //会話関連
+    [SerializeField]
+    private Message messageScript;
+
+    [Header("EnemyMassage")]
+    [SerializeField]
+    public string[] message1;// 表示させるメッセージ
+    public string[] message2;// 表示させるメッセージ
+
+
+    private bool talk1 = true;
+    private bool talk2 = true;
+    //---------------------------------------------------
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +48,57 @@ public class RoamingEnemyScript : MonoBehaviour
             navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
         }
     }
+    //03/30内村追加-----------------------------------------------------------------------------
+
+    private void FixedUpdate()
+    {
+        //会話消去機能----------------------------------------------------
+        if (messageScript.eraseTimeFlag == true)
+        {
+            messageScript.displayTime += Time.deltaTime;
+
+        }
+        if (messageScript.displayTime >= 20.0f)
+        {
+            //Debug.Log("消去フラグがたった");
+            //messageScript.eraseFlag = true;
+            EraseTalk();
+            messageScript.eraseTimeFlag = false;
+            messageScript.displayTime = 0.0f;
+        }
+        //---------------------------------------------------------------------
+
+
+    }
+
+    void EraseTalk()
+    {
+        if (talk1 == false)
+        {
+            talk1 = true;
+        }
+        if (talk2 == false)
+        {
+            talk2 = true;
+        }
+
+        Debug.Log("消去しました");
+        messageScript.messageText.text = "";
+        messageScript.eraseFlag = false;
+
+    }
+
+
+    IEnumerator Message(string[] Conversation)// Messageコルーチン 
+    {
+        yield return new WaitForSeconds(0.01f);// 0.01秒待つ
+        //messageScript.SetMessagePanel(message);// messageScriptのSetMessagePanelを実行する
+
+        messageScript.SetMessagePanel(Conversation);// messageScriptのSetMessagePanelを実行する
+
+    }
+
+    //------------------------------------------------------------------------------------
 
     //Playerを発見した時
     void OnTriggerEnter(Collider other)
@@ -39,6 +106,15 @@ public class RoamingEnemyScript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("発見した!!");
+            if (talk2 == true)
+            {
+                StartCoroutine("Message", message2);// Messageコルーチンを実行する
+                messageScript.talkflag = true;
+                messageScript.eraseTimeFlag = true;
+
+                talk2 = false;
+            }
+
         }
     }
     void OnTriggerStay(Collider other)
