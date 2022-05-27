1e64b025f2a4be8b4d8efa99a5cb3bccbc2e7752
DDDDDihe <ddddd925848962@gmail.com>
2022-05-27 16:38:34 +0900

    ステージ3のドアのアニメーション修正ー＞ジャク・コウ

--- NGT2022UniKP/Assets/Scripts/FenceScript.cs
+++ NGT2022UniKP/Assets/Scripts/FenceScript.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class FenceScript : MonoBehaviour
 {
     public GameObject key;
+    Animator Ani_fence_2;
+    string state;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         key = GameObject.Find("Key");
+        Ani_fence_2 = GetComponent<Animator>();
+        Ani_fence_2.speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (key.GetComponent<KeyScript>().GetKey == true)
-        {
-            if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        //if (key.GetComponent<KeyScript>().GetKey)
+        //{
+            if (other.CompareTag("Player") && Input.GetKey(KeyCode.E) || Input.GetButton("ControllerA"))
             //if (other.CompareTag("Player") && Input.GetButton("ControllerA"))
-
             {
-                this.gameObject.SetActive(false);
-            }
+                //``doahiraku
+                //this.gameObject.SetActive(false);
+                if (Ani_fence_2.speed == 0)
+                {
+                    Ani_fence_2.speed = 1;
+                }
 
-        }
+                Ani_fence_2.SetBool(state, true);
+            }
+        //}
     }
 }
 
